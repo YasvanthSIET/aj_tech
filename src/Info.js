36c9a9502a56f53a511b2e0808477839f6d5f246
YasvanthSIET <yashvj1001@gmail.com>
2023-11-25 20:33:00 +0530

    updated

--- src/Info.js
+++ src/Info.js
@@ -13,8 +13,8 @@ const Info = () => {
                 <div className="col-md-8 col-xl-3" id="con">
                     <div>
                         <h5>About AJ InfoTeck</h5>
-                        <p>Dominate the Digital Landscape with AJ InfoTeck 's Expert Marketing Strategies. Your Gateway to Digital Success Find out the key to the World</p>
-                        <p>Contact :- +91 8122788484</p>
+                        <p className="detail">Dominate the Digital Landscape with AJ InfoTeck 's Expert Marketing Strategies. Your Gateway to Digital Success Find out the key to the World</p>
+                        <p className="detail">Contact :- +91 8122788484</p>
                         <div id="img2">
                             <span><img src={facebook} alt="facebook" className="me-2"/></span>
                             <span><img src={instagram} alt="instagram" /></span>
@@ -24,7 +24,7 @@ const Info = () => {
                 <div className="col-md-4 col-xl-3" id="con">
                     <div>
                         <h5>Quick Links</h5>
-                        <ul>
+                        <ul className="detail">
                             <li>Home</li>
                             <li>About</li>
                             <li>Digital marketing</li>
@@ -37,7 +37,7 @@ const Info = () => {
                 <div className="col-md-8 col-xl-3" id="con">
                     <div>
                         <h5>Address</h5>
-                        <address>13/1A, 2nd Floor,<br />STC College Road,<br />Perumalpuram,<br />Tirunelveli - 627 007,<br />Tamil Nadu.</address>
+                        <address className="detail">13/1A, 2nd Floor,<br />STC College Road,<br />Perumalpuram,<br />Tirunelveli - 627 007,<br />Tamil Nadu.</address>
                     </div>
                 </div>
             </div>
